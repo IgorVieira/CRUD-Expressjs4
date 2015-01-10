a013c07c2914ce11ab6975a0ea8b1cbb2ee3429a
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-01-10 18:02:29 -0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,8 @@ module.exports = function(app){
 	app.post('/user',user.insert);
 	app.get('/user/edit/:id',user.edit);
 	app.post('/user/edit/:id', user.update);
+	app.get('/user/file/:id', user.file);
+	app.post('/user/delete/:id', user.delete);
 
 
 }
